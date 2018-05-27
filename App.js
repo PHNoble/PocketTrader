8ac350f77545ced9264c4a6704cb187f0352019e
Paul Noble <npaul98@vt.edu>
2018-05-27 19:43:18 -0400

    UI stuff.

--- App.js
+++ App.js
@@ -8,51 +8,35 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
-  View
 } from 'react-native';
+import getTheme from './native-base-theme/components';
+import theem from './native-base-theme/variables/theem';
+import Home from './js/view/Home.js';
+import {View, StyleProvider, Root} from 'native-base';
+import { createStackNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <StyleProvider style={getTheme(theem)}>
+        <Root>
+          <RootStack />
+        </Root>
+      </StyleProvider>  
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const RootStack = createStackNavigator({
+  Main: {
+    screen: Home,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}, {
+  navigationOptions: {
+    header: null,
+  }
+}
+);
+
+
---
+++ js/view/Home.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  ScrollView,
+  Dimensions
+} from 'react-native';
+import {View, H1, StyleProvider, Button, Text, Container, Header, Content, Left, Body, Right, Icon, Title, Card, CardItem, Spinner, Form, Input, Item, Fab} from 'native-base';
+import { createStackNavigator } from 'react-navigation';
+
+
+const {width, height} = Dimensions.get('window');
+
+const NavHeader = ({}) => (
+  <Header>
+    <Left style={{flex: 1}}>
+      <Button
+        transparent
+        onPress={() => this.props.navigation.navigate("DrawerOpen")}>
+        <Icon name="menu" />
+      </Button>
+    </Left>
+    <Body style={{flex: 1}}>
+      <Title>HomeScreen</Title>
+    </Body>
+    <Right style={{flex: 1}} />
+  </Header>
+);
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      loading: false,
+      selectedTicker: 'none', // NONE, or given ticker
+      graphType: 'Day', // DAY, WEEK, MONTH, YEAR
+      fabActive: false,
+      inputText: '',
+    }
+  }
+
+
+  render() {
+    const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+    return (
+      <Container style={{backgroundColor: '#FFFDF9'}} >
+        <NavHeader />
+        {/*END HEADER BEGIN CONTENT*/}
+        <Content scrollEnabled={false}>
+          {/*CARD CONTENT BEGIN*/}
+          <H1>Some title???</H1>
+          {/*BEGIN GRAPH DISPLAY CARD*/}
+          {!this.state.loaded && 
+          <Card>
+            <CardItem header>
+              <Icon name="stats" />
+              <Text>Prediction on {this.state.selectedTicker} for {this.state.graphType}</Text>
+              
+            </CardItem>
+            <CardItem>
+              <Body>
+                <ScrollView 
+                  pagingEnabled={true}
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                >
+                  <View style={{width: width}}>
+                    
+                  </View>
+                  <View style={{width: width}}>
+                    <Text>WEEK</Text>
+                  </View>
+                  <View style={{width: width}}>
+                    <Text>MONTH</Text>
+                  </View>
+                  <View style={{width: width}}>
+                    <Text>YEAR</Text>
+                  </View>
+                </ScrollView>
+              </Body>
+            </CardItem>
+          </Card>}
+          {/*ACTIVITY INDICATOR*/}
+          {this.state.loading && this.state.selectedTicker != 'none' &&
+            <Spinner />
+          }
+          {/*Search Form*/}
+          <Card>
+            <CardItem header>
+              <Text>Input a stock ticker</Text>
+            </CardItem>
+            <CardItem style={{width: '100%'}}>
+              <Form style={{width: '100%'}}>
+                <Item rounded>
+                  <Input 
+                  placeholder={'Search Tickers'}
+                  onChangeText={(text) => {this.setState({inputText: text})}}
+                  autoCorrect={'none'}
+                  autoCapitalize={'none'}
+                  > 
+                  </Input>
+                </Item>
+              </Form>n
+            </CardItem>
+            <CardItem>
+              <Button block disabled={this.state.inputText == '' || this.state.inputText == null} 
+                style={{width: '100%'}}
+                onPress={() => {
+                  this.setState({
+                    selectedTicker: this.state.inputText,
+                    loading: true,});
+                }}>
+                <Text>
+                  Search
+                </Text>
+              </Button>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  }
+}
+
